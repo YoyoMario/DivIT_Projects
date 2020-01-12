26f4911151d0788c3e1d9c8512f219929053c8b5
YoyoMario <mario.haberle@gmail.com>
2020-01-12 10:57:29 +0100

    ADded commands to switch the relay.

--- DoorBot5000/Arduino/Arduino_GetRequest/Arduino_GetRequest.ino
+++ DoorBot5000/Arduino/Arduino_GetRequest/Arduino_GetRequest.ino
@@ -6,9 +6,13 @@ const char * ssid = "Dev DeltaR"; // replace with your wifi ssid and wpa2 key
 const char * pass = "dajnet66";
 const char * url = "http://office.divit.hr:3001/doorbot";
 
+uint8_t LED_Pin = D8; 
+
 WiFiClient client;
 
 void setup() {
+    pinMode(LED_Pin, OUTPUT);
+    
     Serial.begin(9600);
     delay(10);
 
@@ -27,17 +31,39 @@ void setup() {
 }
 
 void loop() {
+    //Gets buzzer status.
+    int value = GetRequest();
+
+    //Check if we want to buzz the door.
+    if(value == 1){
+        digitalWrite(LED_Pin, HIGH);
+        delay(1000);
+        digitalWrite(LED_Pin, LOW);
+    }
+    
+    delay(1000);
+}
+
+int GetRequest(){
     HTTPClient http;
     http.begin(url);
     int httpCode = http.GET();
     String payload = http.getString();
-
-    Serial.println("---------------------------------------------------------------------------------");
-    Serial.println(httpCode);
-    Serial.println(payload);
-    Serial.println("---------------------------------------------------------------------------------");
-    
     http.end();
+    
+    //Serial.println("---------------------------------------------------------------------------------");
+    //Serial.println(httpCode);
+    //Serial.println(payload);
+    //Serial.println("---------------------------------------------------------------------------------");
+    
+    if(httpCode == -1){
+        return 0;
+    }
 
-    delay(1000);
+    if(payload == "NO"){
+        return 0;
+    }
+    else if(payload == "YES"){
+        return 1;
+    }    
 }
